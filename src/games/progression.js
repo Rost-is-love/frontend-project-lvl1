9f4793a8ddcb22fb83586c0d20b3bf7a4c87b12d
Rostislav Antonov <antonov.qwertylox004@yandex.ru>
2020-10-26 17:07:26 +0300

    make brain progression game

---
+++ src/games/progression.js
@@ -0,0 +1,34 @@
+import engine from '../index.js';
+
+const rules = () => console.log('What number is missing in the progression?');
+
+const makeProgression = (amountOfNumbers, startNumber, indexOfMissingNumber,
+  progressionInterval) => {
+  let progression = '';
+  let currentNum = startNumber;
+  let missingNumber;
+
+  for (let i = 0; i < amountOfNumbers; i += 1) {
+    if (i === indexOfMissingNumber) {
+      progression = `${progression}.. `;
+      missingNumber = currentNum;
+    } else progression = `${progression}${currentNum} `;
+    currentNum += progressionInterval;
+  }
+
+  return [progression, missingNumber];
+};
+
+const gamesСonditions = () => {
+  const amountOfNumbers = 10;
+  const startNumber = Math.floor(Math.random() * 10);
+  const indexOfMissingNumber = Math.floor(Math.random() * 10);
+  const progressionInterval = Math.ceil(Math.random() * 10);
+  const [question, missingNumber] = makeProgression(amountOfNumbers, startNumber,
+    indexOfMissingNumber, progressionInterval);
+  const correctAnswer = String(missingNumber);
+
+  return [question, correctAnswer];
+};
+
+export default () => engine(rules, gamesСonditions);
